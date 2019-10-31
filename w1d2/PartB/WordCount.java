c1e53b5625c133466d6c41aca9ba59a8fd9183b0
scug <scug@VXN-MacPro-3.local>
2019-10-30 23:22:22 -0500

    d2

--- w1d2/PartB/WordCount.java
+++ w1d2/PartB/WordCount.java
@@ -5,49 +5,42 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WordCount {
-	private List<Mapper> mappers;
-	private List<Reducer> reducers;
-	private int m;
-	private int r;
-	
-	WordCount(){
-		mappers = new ArrayList<>(Arrays.asList(
-				                          new Mapper("//src//bigData//w1d2//PartB//textfile/testData1.txt"),
-				                          new Mapper("//src//bigData//w1d2//PartB//textfile/testData2.txt"),
-				                          new Mapper("//src//bigData//w1d2//PartB//textfile/testData3.txt")));
-		reducers = new ArrayList<>(Arrays.asList(new Reducer(),
-				                                 new Reducer(),
-				                                 new Reducer(),
-				                                 new Reducer()));
+		private List<Mapper> mappers;
+		private List<Reducer> reducers;
+		private int m;
+		private int r;
+
+	WordCount() {
+		mappers = new ArrayList<>(Arrays.asList(new Mapper("//src//bigData//w1d2//PartB//textfile/testData1.txt"),
+				new Mapper("//src//bigData//w1d2//PartB//textfile/testData2.txt"),
+				new Mapper("//src//bigData//w1d2//PartB//textfile/testData3.txt")));
+		reducers = new ArrayList<>(Arrays.asList(new Reducer(), new Reducer(), new Reducer(), new Reducer()));
 		m = mappers.size();
 		r = reducers.size();
 	}
-	
+
 	public List<List<Pair<String, Integer>>> jobTracker() {
 		List<List<Pair<String, Integer>>> mapperOutputs = new ArrayList<>();
-		List<Pair<String, Integer>> mapperOutput1 = mappers.get(0)
-				.createListPairs(mappers.get(0).readFromFile());
+		List<Pair<String, Integer>> mapperOutput1 = mappers.get(0).createListPairs(mappers.get(0).readFromFile());
 		mapperOutputs.add(mapperOutput1);
 		System.out.println("Mapper 0 Output");
 		mapperOutput1.forEach(System.out::print);
 		System.out.println();
-		
-		List<Pair<String, Integer>> mapperOutput2 = mappers.get(1)
-				.createListPairs(mappers.get(1).readFromFile());
+
+		List<Pair<String, Integer>> mapperOutput2 = mappers.get(1).createListPairs(mappers.get(1).readFromFile());
 		mapperOutputs.add(mapperOutput2);
 		System.out.println("Mapper 1 Output");
 		mapperOutput2.forEach(System.out::print);
 		System.out.println();
 
-		List<Pair<String, Integer>> mapperOutput3 = mappers.get(2)
-				.createListPairs(mappers.get(2).readFromFile());
+		List<Pair<String, Integer>> mapperOutput3 = mappers.get(2).createListPairs(mappers.get(2).readFromFile());
 		mapperOutputs.add(mapperOutput3);
 		System.out.println("Mapper 2 Output");
 		mapperOutput3.forEach(System.out::print);
 		System.out.println();
 		return mapperOutputs;
 	}
-	
+
 	public List<List<Pair<String, Integer>>> pairsSentToReducers() {
 		List<List<Pair<String, Integer>>> reducerInputs = new ArrayList<>();
 		List<List<Pair<String, Integer>>> Mappers = jobTracker();
@@ -56,33 +49,33 @@ public class WordCount {
 		List<Pair<String, Integer>> reducerInputs2 = new ArrayList<>();
 		List<Pair<String, Integer>> reducerInputs3 = new ArrayList<>();
 
-		for(List<Pair<String, Integer>> mapperOutput: Mappers) {
+		for (List<Pair<String, Integer>> mapperOutput : Mappers) {
 			List<Pair<String, Integer>> reducerInput0 = new ArrayList<>();
 			List<Pair<String, Integer>> reducerInput1 = new ArrayList<>();
 			List<Pair<String, Integer>> reducerInput2 = new ArrayList<>();
 			List<Pair<String, Integer>> reducerInput3 = new ArrayList<>();
-			for(Pair<String, Integer> p: mapperOutput) {
-				if(getPartition(p.getK())==0) {
+			for (Pair<String, Integer> p : mapperOutput) {
+				if (getPartition(p.getK()) == 0) {
 					reducerInput0.add(p);
 					reducerInputs0.add(p);
-				}else if(getPartition(p.getK())==1) {
+				} else if (getPartition(p.getK()) == 1) {
 					reducerInput1.add(p);
 					reducerInputs1.add(p);
-				}else if(getPartition(p.getK())==2) {
+				} else if (getPartition(p.getK()) == 2) {
 					reducerInput2.add(p);
 					reducerInputs2.add(p);
-				}else if(getPartition(p.getK())==3) {
+				} else if (getPartition(p.getK()) == 3) {
 					reducerInput3.add(p);
 					reducerInputs3.add(p);
 				}
 			}
-			System.out.println("Pairs sent from Mapper "+Mappers.indexOf(mapperOutput)+ " Reducer 0");
+			System.out.println("Pairs sent from Mapper " + Mappers.indexOf(mapperOutput) + " Reducer 0");
 			reducerInput0.forEach(System.out::print);
-			System.out.println("Pairs sent from Mapper "+Mappers.indexOf(mapperOutput)+ " Reducer 1");
+			System.out.println("Pairs sent from Mapper " + Mappers.indexOf(mapperOutput) + " Reducer 1");
 			reducerInput1.forEach(System.out::print);
-			System.out.println("Pairs sent from Mapper "+Mappers.indexOf(mapperOutput)+ " Reducer 2");
+			System.out.println("Pairs sent from Mapper " + Mappers.indexOf(mapperOutput) + " Reducer 2");
 			reducerInput2.forEach(System.out::print);
-			System.out.println("Pairs sent from Mapper "+Mappers.indexOf(mapperOutput)+ " Reducer 3");
+			System.out.println("Pairs sent from Mapper " + Mappers.indexOf(mapperOutput) + " Reducer 3");
 			reducerInput3.forEach(System.out::print);
 		}
 		reducerInputs.add(reducerInputs0);
@@ -91,43 +84,45 @@ public class WordCount {
 		reducerInputs.add(reducerInputs3);
 		return reducerInputs;
 	}
-	
-	public int getPartition(String key){
-		return Math.abs(key.hashCode()% r);
+
+	public int getPartition(String key) {
+		return Math.abs(key.hashCode() % r);
 	}
-	
-	public static void main(String[] args) {
-		WordCount w = new WordCount();
-		w.jobTracker();
-		w.pairsSentToReducers();
-		List<List<Pair<String, Integer>>> listOfPairsSentToReducers = w.pairsSentToReducers();
+
+	public void shuffleReduce() {
+		List<List<Pair<String, Integer>>> listOfPairsSentToReducers = pairsSentToReducers();
 		List<List<GroupByPair<String, List<Integer>>>> reducerInputs = new ArrayList<>();
 
-		for(List<Pair<String, Integer>> lp: listOfPairsSentToReducers) {
-			List<GroupByPair<String, List<Integer>>> reducerInput= new ArrayList<>(w.reducers.get(listOfPairsSentToReducers
-					.indexOf(lp)).generateReducerInput(lp));
+		for (List<Pair<String, Integer>> lp : listOfPairsSentToReducers) {
+			List<GroupByPair<String, List<Integer>>> reducerInput = new ArrayList<>(
+					reducers.get(listOfPairsSentToReducers.indexOf(lp)).generateReducerInput(lp));
 			reducerInputs.add(reducerInput);
 		}
-		for(List<GroupByPair<String, List<Integer>>> r: reducerInputs) {
-			System.out.println("Reducer "+ reducerInputs.indexOf(r) +" input");
+		for (List<GroupByPair<String, List<Integer>>> r : reducerInputs) {
+			System.out.println("Reducer " + reducerInputs.indexOf(r) + " input");
 			r.forEach(System.out::print);
 		}
-		
+
 		List<List<GroupByPair<String, Integer>>> reducerOutputs = new ArrayList<>();
-		for(List<GroupByPair<String, List<Integer>>> gp: reducerInputs) {
-			List<GroupByPair<String, Integer>> reducerOutput= new ArrayList<>(w.reducers.get(reducerInputs
-					.indexOf(gp)).generateReducerOutput(gp));
+		for (List<GroupByPair<String, List<Integer>>> gp : reducerInputs) {
+			List<GroupByPair<String, Integer>> reducerOutput = new ArrayList<>(
+					reducers.get(reducerInputs.indexOf(gp)).generateReducerOutput(gp));
 			reducerOutputs.add(reducerOutput);
 		}
-		for(List<GroupByPair<String, Integer>> r: reducerOutputs) {
-			System.out.println("Reducer "+ reducerOutputs.indexOf(r) +" output");
+		for (List<GroupByPair<String, Integer>> r : reducerOutputs) {
+			System.out.println("Reducer " + reducerOutputs.indexOf(r) + " output");
 			r.forEach(System.out::print);
 		}
+	}
+	public static void main(String[] args) {
+		WordCount w = new WordCount();
+		w.shuffleReduce();
 	}
 
 }
-/**Sample output
-*
+/**
+ * Sample output
+ *
 Mapper 0 Output
 < bat, 1 >
 < cat, 1 >
@@ -164,159 +159,45 @@ Mapper 2 Output
 < wat, 1 >
 < zat, 1 >
 
-Mapper 0 Output
-< bat, 1 >
-< cat, 1 >
-< cat, 1 >
-< eat, 1 >
-< fat, 1 >
-< mat, 1 >
-< mat, 1 >
-< pat, 1 >
-< rat, 1 >
-< sat, 1 >
-
-Mapper 1 Output
-< bat, 1 >
-< eat, 1 >
-< hat, 1 >
-< hat, 1 >
-< mat, 1 >
-< oat, 1 >
-< oat, 1 >
-< pat, 1 >
-< pat, 1 >
-< rat, 1 >
-
-Mapper 2 Output
-< cat, 1 >
-< hat, 1 >
-< jat, 1 >
-< kat, 1 >
-< lat, 1 >
-< pat, 1 >
-< rat, 1 >
-< sat, 1 >
-< wat, 1 >
-< zat, 1 >
-
-Pairs send from Mapper 0 Reducer 0
+Pairs sent from Mapper 0 Reducer 0
 < eat, 1 >
 < mat, 1 >
 < mat, 1 >
-Pairs send from Mapper 0 Reducer 1
+Pairs sent from Mapper 0 Reducer 1
 < bat, 1 >
 < fat, 1 >
 < rat, 1 >
-Pairs send from Mapper 0 Reducer 2
+Pairs sent from Mapper 0 Reducer 2
 < cat, 1 >
 < cat, 1 >
 < sat, 1 >
-Pairs send from Mapper 0 Reducer 3
+Pairs sent from Mapper 0 Reducer 3
 < pat, 1 >
-Pairs send from Mapper 1 Reducer 0
+Pairs sent from Mapper 1 Reducer 0
 < eat, 1 >
 < mat, 1 >
-Pairs send from Mapper 1 Reducer 1
+Pairs sent from Mapper 1 Reducer 1
 < bat, 1 >
 < rat, 1 >
-Pairs send from Mapper 1 Reducer 2
+Pairs sent from Mapper 1 Reducer 2
 < oat, 1 >
 < oat, 1 >
-Pairs send from Mapper 1 Reducer 3
+Pairs sent from Mapper 1 Reducer 3
 < hat, 1 >
 < hat, 1 >
 < pat, 1 >
 < pat, 1 >
-Pairs send from Mapper 2 Reducer 0
-Pairs send from Mapper 2 Reducer 1
+Pairs sent from Mapper 2 Reducer 0
+Pairs sent from Mapper 2 Reducer 1
 < jat, 1 >
 < rat, 1 >
 < zat, 1 >
-Pairs send from Mapper 2 Reducer 2
+Pairs sent from Mapper 2 Reducer 2
 < cat, 1 >
 < kat, 1 >
 < sat, 1 >
 < wat, 1 >
-Pairs send from Mapper 2 Reducer 3
-< hat, 1 >
-< lat, 1 >
-< pat, 1 >
-Mapper 0 Output
-< bat, 1 >
-< cat, 1 >
-< cat, 1 >
-< eat, 1 >
-< fat, 1 >
-< mat, 1 >
-< mat, 1 >
-< pat, 1 >
-< rat, 1 >
-< sat, 1 >
-
-Mapper 1 Output
-< bat, 1 >
-< eat, 1 >
-< hat, 1 >
-< hat, 1 >
-< mat, 1 >
-< oat, 1 >
-< oat, 1 >
-< pat, 1 >
-< pat, 1 >
-< rat, 1 >
-
-Mapper 2 Output
-< cat, 1 >
-< hat, 1 >
-< jat, 1 >
-< kat, 1 >
-< lat, 1 >
-< pat, 1 >
-< rat, 1 >
-< sat, 1 >
-< wat, 1 >
-< zat, 1 >
-
-Pairs send from Mapper 0 Reducer 0
-< eat, 1 >
-< mat, 1 >
-< mat, 1 >
-Pairs send from Mapper 0 Reducer 1
-< bat, 1 >
-< fat, 1 >
-< rat, 1 >
-Pairs send from Mapper 0 Reducer 2
-< cat, 1 >
-< cat, 1 >
-< sat, 1 >
-Pairs send from Mapper 0 Reducer 3
-< pat, 1 >
-Pairs send from Mapper 1 Reducer 0
-< eat, 1 >
-< mat, 1 >
-Pairs send from Mapper 1 Reducer 1
-< bat, 1 >
-< rat, 1 >
-Pairs send from Mapper 1 Reducer 2
-< oat, 1 >
-< oat, 1 >
-Pairs send from Mapper 1 Reducer 3
-< hat, 1 >
-< hat, 1 >
-< pat, 1 >
-< pat, 1 >
-Pairs send from Mapper 2 Reducer 0
-Pairs send from Mapper 2 Reducer 1
-< jat, 1 >
-< rat, 1 >
-< zat, 1 >
-Pairs send from Mapper 2 Reducer 2
-< cat, 1 >
-< kat, 1 >
-< sat, 1 >
-< wat, 1 >
-Pairs send from Mapper 2 Reducer 3
+Pairs sent from Mapper 2 Reducer 3
 < hat, 1 >
 < lat, 1 >
 < pat, 1 >
@@ -358,4 +239,6 @@ Reducer 3 output
 < hat, 3 >
 < lat, 1 >
 < pat, 4 >
-*/
+
+*
+ */
